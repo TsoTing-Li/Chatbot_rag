907af90d842e576257637678708a66b47a94cda4
Steven <tsoting_li@innodisk.com>
2024-09-02 11:01:33 +0800

    fix: remove HF token

--- tools/setting.py
+++
@@ -1,9 +0,0 @@
-HUGGING_FACE_TOKEN = {
-    "meta-llama/Meta-Llama-3-8B": "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",
-    "unsloth/llama-3-8b-bnb-4bit": "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",
-    "meta-llama/Meta-Llama-3.1-8B-Instruct": "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",
-    "HuggingFaceH4/zephyr-7b-beta": "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",
-    "meta-llama/Llama-Guard-3-8B": "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",
-    "meta-llama/Llama-2-7b-chat-hf": "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",
-    "meta-llama/Llama-2-7b-hf": "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",
-}
